c223effdb030c97ededf5eac49eda138d7a0a503
Olivier Cortès <olive@licorn.org>
2013-04-19 00:27:03 +0200

    Add Python 2/3 compatibility, and readability.

--- powerline-shell.py
+++ powerline-shell.py
@@ -210,49 +210,78 @@ def add_hg_segment(powerline, cwd):
 def get_git_status():
     has_pending_commits = True
     has_untracked_files = False
-    origin_position = ""
-    output = subprocess.Popen(['git', 'status', '--ignore-submodules'],
-                              stdout=subprocess.PIPE).communicate()[0]
-    for line in output.split('\n'):
-        origin_status = re.findall(
-                r"Your branch is (ahead|behind).*?(\d+) comm", line)
-        if origin_status:
-            origin_position = " %d" % int(origin_status[0][1])
-            if origin_status[0][0] == 'behind':
-                origin_position += u'\u21E3'
-            if origin_status[0][0] == 'ahead':
-                origin_position += u'\u21E1'
+    origin_position = ''
 
-        if line.find('nothing to commit') >= 0:
-            has_pending_commits = False
-        if line.find('Untracked files') >= 0:
-            has_untracked_files = True
+    if sys.version_info[0] <= 2:
+        output = subprocess.Popen(['git', 'status', '--ignore-submodules'],
+                                  stdout=subprocess.PIPE).stdout
+
+    else:
+        output = subprocess.Popen(['git', 'status', '--ignore-submodules'],
+                                  stdout=subprocess.PIPE,
+                                  universal_newlines=True).stdout.read()
+
+        for line in output.split('\n'):
+            origin_status = re.findall(
+                    r"Your branch is (ahead|behind).*?(\d+) comm", line)
+
+            if origin_status:
+                origin_position = " %d" % int(origin_status[0][1])
+
+                if origin_status[0][0] == 'behind':
+                    origin_position += u'\u21E3'
+
+                if origin_status[0][0] == 'ahead':
+                    origin_position += u'\u21E1'
+
+            if line.find('nothing to commit') >= 0:
+                has_pending_commits = False
+
+            if line.find('Untracked files') >= 0:
+                has_untracked_files = True
+
     return has_pending_commits, has_untracked_files, origin_position
 
 
 def add_git_segment(powerline, cwd):
     #cmd = "git branch 2> /dev/null | grep -e '\\*'"
-    p1 = subprocess.Popen(['git', 'branch'], stdout=subprocess.PIPE,
+
+    if sys.version_info[0] <= 2:
+        kw = {}
+
+    else:
+        kw = {'universal_newlines': True}
+
+    p1 = subprocess.Popen(['git', 'branch'],
+                          stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE)
-    p2 = subprocess.Popen(['grep', '-e', '\\*'], stdin=p1.stdout,
-                          stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(['grep', '-e', '\\*'],
+                          stdin=p1.stdout,
+                          stdout=subprocess.PIPE, **kw)
+
     output = p2.communicate()[0].strip()
+
     if not output:
         return False
 
     branch = output.rstrip()[2:]
+
     has_pending_commits, has_untracked_files, origin_position = get_git_status()
+
     branch += origin_position
+
     if has_untracked_files:
         branch += ' +'
 
     bg = Color.REPO_CLEAN_BG
     fg = Color.REPO_CLEAN_FG
+
     if has_pending_commits:
         bg = Color.REPO_DIRTY_BG
         fg = Color.REPO_DIRTY_FG
 
     powerline.append(Segment(powerline, ' %s ' % branch, fg, bg))
+
     return True
 
 
